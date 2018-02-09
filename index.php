a21bb5a67dbd2a6b94beb70948f73ff7eb9d3ffd
andres <titocanul7@live.com.mx>
2018-02-09 10:22:02 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>	Document</title>
+ 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimun-scale=1.0">
+ 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ 	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<h1>
+				Lets
+			</h1>
+		</div>
+	</header>
+	<div class="container">
+		<section class="main row">
+		<article class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+			<p>
+				Overview
+				LETS America, Inc. is a Multidisciplinary Engineering Design and Data Analytics Consulting company. LETS has been in business for more than 20 years, supporting its fortune 500 National and International customer bases in the telecom, oil & gas and infrastructure sectors. LETS is one of the most cost effective and reliable service provider in the area of Engineering design and Consulting knowledge processes with presence in three continents.
+			</p>
+		</article>
+		<aside class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+			<p>
+				Overview
+				LETS America, Inc. is a Multidisciplinary Engineering Design and Data Analytics Consulting company. LETS has been in business for more than 20 years, supporting its fortune 500 National and International customer bases in the telecom, oil & gas and infrastructure sectors. LETS is one of the most cost effective and reliable service provider in the area of Engineering design and Consulting knowledge processes with presence in three continents.
+			</p>
+		</aside>
+	</section>
+	</div>
+	
+	<button class="btn btn-primary">Button</button>
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
